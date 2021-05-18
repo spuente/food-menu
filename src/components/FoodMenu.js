4e806195178d98c1a50f7430ee65711c2f93d16b
Sebastian Puente <sebas7pr@gmail.com>
2021-05-18 00:35:46 -0500

    refactor: display all food menu items with cards

--- src/components/FoodMenu.js
+++ src/components/FoodMenu.js
@@ -1,26 +1,7 @@
-import {
-  Button,
-  Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Grid,
-  IconButton,
-  Typography,
-} from "@material-ui/core"
-import { makeStyles } from "@material-ui/core/styles"
-import { Favorite } from "@material-ui/icons"
+import { Grid } from "@material-ui/core"
 import FoodCard from "./FoodCard"
 
-const useStyles = makeStyles({
-  media: {
-    height: 200,
-  },
-})
-
 const FoodMenu = () => {
-  const classes = useStyles()
   return (
     <Grid container spacing={3}>
       <Grid item xs={12} sm={6} md={4}>
@@ -31,54 +12,18 @@ const FoodMenu = () => {
         />
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
-        <Card>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image="/pics/hamburguer.jpg"
-              title="Hamburguer"
-            />
-            <CardContent>
-              <Typography variant="h5" component="h2">
-                Hamburguer
-              </Typography>
-              <Typography>Hamburguer description</Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button
-              onClick={() => alert("You like Hamburguer")}
-              color="primary"
-            >
-              Like
-            </Button>
-          </CardActions>
-        </Card>
+        <FoodCard
+          title="Hamburguer"
+          description="Hamburguer description"
+          imageUrl="/pics/hamburguer.jpg"
+        />
       </Grid>
       <Grid item xs={12} sm={6} md={4}>
-        <Card>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image="/pics/fries.jpg"
-              title="French Fries"
-            />
-            <CardContent>
-              <Typography variant="h5" component="h2">
-                French Fries
-              </Typography>
-              <Typography>French Fries description</Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <IconButton
-              onClick={() => alert("You like French Fries")}
-              color="default"
-            >
-              <Favorite />
-            </IconButton>
-          </CardActions>
-        </Card>
+        <FoodCard
+          title="French Fries"
+          description="French Fries description"
+          imageUrl="/pics/fries.jpg"
+        />
       </Grid>
     </Grid>
   )
