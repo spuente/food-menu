7a5e662e3f4f92134c8784c1a6b952ab40fb7d22
Sebastian Puente <sebas7pr@gmail.com>
2021-05-18 00:46:21 -0500

    refactor: spread all food props for FoodCard

--- src/components/FoodMenu.js
+++ src/components/FoodMenu.js
@@ -5,13 +5,9 @@ import foodList from "../data/foodList"
 const FoodMenu = () => {
   return (
     <Grid container spacing={3}>
-      {foodList.map(({ title, description, imageUrl }) => (
-        <Grid item key={title} xs={12} sm={6} md={4}>
-          <FoodCard
-            title={title}
-            description={description}
-            imageUrl={imageUrl}
-          />
+      {foodList.map((food) => (
+        <Grid item key={food.title} xs={12} sm={6} md={4}>
+          <FoodCard {...food} />
         </Grid>
       ))}
     </Grid>
